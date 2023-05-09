ae99322f190b8f4203a55328c1b390ad925fc381
Nguyen Thuy Linh <thuylinh2409nguyen@gmail.com>
2023-05-09 22:42:18 +0700

    Upload BaseTest for headless browser/coccoc/opera/brave

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,8 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseTest {
@@ -27,26 +30,35 @@ public class BaseTest {
 			driver = new EdgeDriver();
 			break;
 		case "opera":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\operadriver.exe");
+			System.setProperty("webdriver.opera.driver", projectPath + "\\browserDrivers\\operadriver.exe");
 			driver = new OperaDriver();
 			break;
 		case "coccoc":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver_110.0.5481.77.exe");
+			ChromeOptions optionsCocCoc = new ChromeOptions();
+			optionsCocCoc.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
+			driver = new ChromeDriver(optionsCocCoc);
+			break;
+		case "brave":
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			ChromeOptions optionsBrave = new ChromeOptions();
+			optionsBrave.setBinary("C:\\Program Files\\Brave\\Browser\\Application\\browser.exe");
+			driver = new ChromeDriver(optionsBrave);
 			break;
 		case "h_chrome":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			ChromeOptions optionsChrome = new ChromeOptions();
+			optionsChrome.addArguments("-headless");
+			optionsChrome.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(optionsChrome);
 			break;
 		case "h_firefox":
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
+			FirefoxOptions optionsFirefox = new FirefoxOptions();
+			optionsFirefox.addArguments("-headless");
+			optionsFirefox.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(optionsFirefox);
 			break;
-		case "h_edge":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			break;
-
 		default:
 			throw new RuntimeException("Browser Name Invalid");
 		}
