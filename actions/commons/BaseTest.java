d98344ecaa10a1921d630eb717b29a7044ded665
Nguyen Thuy Linh <thuylinh2409nguyen@gmail.com>
2023-05-09 23:09:39 +0700

    Update getBrowserDriver function using WebDriverManager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 //import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	WebDriver driver;
@@ -18,42 +21,42 @@ public class BaseTest {
 	protected WebDriver getBrowserDriver(String browserName) {
 		switch (browserName) {
 		case "firefox":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 			break;
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 			break;
 		case "edge":
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 			break;
 		case "opera":
-			System.setProperty("webdriver.opera.driver", projectPath + "\\browserDrivers\\operadriver.exe");
+			WebDriverManager.operadriver().setup();
 			driver = new OperaDriver();
 			break;
 		case "coccoc":
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver_110.0.5481.77.exe");
+			WebDriverManager.chromedriver().driverVersion("110.0.5481.77").setup();
 			ChromeOptions optionsCocCoc = new ChromeOptions();
 			optionsCocCoc.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
 			driver = new ChromeDriver(optionsCocCoc);
 			break;
 		case "brave":
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			ChromeOptions optionsBrave = new ChromeOptions();
 			optionsBrave.setBinary("C:\\Program Files\\Brave\\Browser\\Application\\browser.exe");
 			driver = new ChromeDriver(optionsBrave);
 			break;
 		case "h_chrome":
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			ChromeOptions optionsChrome = new ChromeOptions();
 			optionsChrome.addArguments("-headless");
 			optionsChrome.addArguments("window-size=1920x1080");
 			driver = new ChromeDriver(optionsChrome);
 			break;
 		case "h_firefox":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			WebDriverManager.firefoxdriver().setup();
 			FirefoxOptions optionsFirefox = new FirefoxOptions();
 			optionsFirefox.addArguments("-headless");
 			optionsFirefox.addArguments("window-size=1920x1080");
