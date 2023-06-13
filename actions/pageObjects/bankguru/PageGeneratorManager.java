46559674f3e8862fb62f5a90d879c214feb7c9c6
Nguyen Thuy Linh <thuylinh2409nguyen@gmail.com>
2023-06-13 21:14:27 +0700

    [BankGuru] Upload NewCustomer - TC01-28

---
+++ actions/pageObjects/bankguru/PageGeneratorManager.java
@@ -0,0 +1,20 @@
+package pageObjects.bankguru;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageGeneratorManager {
+	WebDriver driver;
+	
+	static public NewCustomerPO getNewCustomerPage(WebDriver driver) {
+		return new NewCustomerPO(driver);
+	}
+	
+	static public LoginPO getLoginPage(WebDriver driver) {
+		return new LoginPO(driver);
+	}
+	
+	static public HomePO getHomePage(WebDriver driver) {
+		return new HomePO(driver);
+	}
+	
+}
---
+++ interfaces/bankguru/AbstractUI.java
@@ -0,0 +1,6 @@
+package bankguru;
+
+public class AbstractUI {
+	public static final String LEFT_MENU_BY_LABEL = "xpath=//ul[@class='menusubnav']//a[text()='%s']";
+}
+
